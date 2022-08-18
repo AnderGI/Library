61530cdfcd0cdd7790ce655e9321f56e49937d0e
AnderG <agibarguen@gmail.com>
2022-08-18 17:56:58 +0200

    Book implementation functionality added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const popupEL = document.getElementById('popup')
 const sendPopUp= document.getElementById('submitPopUp')
 const addABook = document.getElementById('addBook')
+const libraryEl = document.getElementById('library')
 
 //MAKE THE POPUP APPEAR
 addABook.addEventListener('click', ()=>{
@@ -11,22 +12,44 @@ sendPopUp.addEventListener('click',()=>{
 })
 
 //CREATE THE BOOK CONSTRUCTOR AND OBJECT
-function Book(title,author,page){
+function Book(title,author,pages){
     this.title=title;
     this.author=author;
-    this.page=page;
+    this.pages=pages;
 }
 Book.prototype.displayBookInfo=function(){
-    return `${this.title} by ${this.author} has ${this.page} pages.`
+    return `${this.title} by ${this.author} has ${this.pages} pages.`
+}
+Book.prototype.addBook = function (){
+
+        let bookCard = document.createElement('div');
+        let cardTitle = document.createElement('p');
+        cardTitle.textContent = `${this.title}`
+        bookCard.append(cardTitle)
+    
+        let cardAuthor = document.createElement('p')
+        cardAuthor.textContent = `${this.author}`
+        bookCard.append(cardAuthor) 
+    
+        let cardPages = document.createElement('p')
+        cardPages.textContent = `${this.pages} pages.`
+        bookCard.append(cardPages) 
+    
+        libraryEl.append(bookCard)
+
+    
 }
 
 
 // DISPLAY THE BOOK
+//ADD A NEW CONTAINER FOR EACH BOOK INSIDE SECTION #Library
+//WHEN SEND IS CLICKED AND THE INPUT VALUE ARE "" IT SHOULDNT WORK
 let author = document.querySelector('#bookAuthor');
 let title = document.querySelector('#bookTitle');
 let pages = document.querySelector('#bookPages');
+let NewBook = new Book ()
 function displayBook(x,y,z){
-    let NewBook = new Book (x, y, z)
+    NewBook = new Book (x, y, z)
     console.log(NewBook.displayBookInfo())
 }
 
@@ -34,4 +57,13 @@ function displayBook(x,y,z){
 sendPopUp.addEventListener('click',(e)=>{
     e.preventDefault()
     displayBook(title.value,author.value,pages.value)
+    NewBook.addBook(title.value,author.value,pages.value)
+    title.value = ""
+    author.value = ""
+    pages.value = ""
+
 })
+
+
+
+
