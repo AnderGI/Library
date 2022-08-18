807498ed79bf7f1d8e3bc77a1352c33155ae29c6
AnderG <agibarguen@gmail.com>
2022-08-18 12:57:04 +0200

    Book display and pushed into myLibrary array

---
+++ script.js
@@ -0,0 +1,32 @@
+//Array to store the books
+// For the boks user prompt
+//OBJECT for author title pages info
+const newBookEl = document.getElementById('newBookBtn')
+const bookDisplayEl = document.getElementById('bookDisplay')
+const inputEl = document.getElementById('bookInput')
+let myLibrary = []
+
+newBookEl.addEventListener('click', addBookToLibrary)
+
+
+function addBookToLibrary(){
+    myLibrary.push(inputEl.value)
+    displayBooks()
+    
+}
+
+function displayBooks(){
+    bookDisplayEl.textContent += myLibrary[myLibrary.length-1]
+}
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+Book.prototype.displayBookInfo = function(){
+    return `
+    ${this.title} by ${this.author} has ${this.pages} pages and I ${this.read}
+    `
+}
