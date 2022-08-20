5f9e6455b099de411ce45c2b41c610dcd306139c
AnderG <agibarguen@gmail.com>
2022-08-20 18:30:56 +0200

    DElete btn associated qith the myLibrary array

--- script.js
+++ script.js
@@ -14,61 +14,53 @@ closeLogInBtn.addEventListener('click',()=>{
     logInDialog.close()
 })
 
-//SAVE NewBook object in myLIbrary array of objects
-let myLibrary = []
-function addBookToLibrary(){
-    myLibrary.push(NewBook)
-    console.log(myLibrary)
-}
-
 
 
 //MAKE THE POPUP APPEAR
 addABook.addEventListener('click', ()=>{
     popupEL.showModal()
 })
-
 sendPopUp.addEventListener('click',()=>{
     popupEL.close()
 })
+
+
+
 //CREATE THE BOOK CONSTRUCTOR AND OBJECT
-// DISPLAY THE BOOK
-//ADD A NEW CONTAINER FOR EACH BOOK INSIDE SECTION #Library
-//WHEN SEND IS CLICKED AND THE INPUT VALUE ARE "" IT SHOULDNT WORK
+
 let author = document.querySelector('#bookAuthor');
 let title = document.querySelector('#bookTitle');
 let pages = document.querySelector('#bookPages');
 
-//BOOK CONSTRUCTOR AND PROTOTYPES FUNCTION
+
 function Book(title,author,pages){
     this.title=title;
     this.author=author;
     this.pages=pages;
 }
 Book.prototype.addBook = function (){
+        //ADD THE BOOK DIV
         let bookCard = document.createElement('div');
         bookCard.setAttribute('class','bookCard')
+            //DISPLAYS THE INFO OF THE BOOK
+            let cardTitle = document.createElement('p');
+            cardTitle.textContent = `${this.title} by ${this.author} has ${this.pages} pages.`
+            bookCard.append(cardTitle)
 
-        let cardTitle = document.createElement('p');
-        cardTitle.textContent = `${this.title} by ${this.author} has ${this.pages} pages.`
-        bookCard.append(cardTitle)
-
-        /*let cardButton = document.createElement('p');*/
-
+            //BUTTON TO SEE IF IT HAS BEEN READ
             let readOrNotBtn = document.createElement('button')
             readOrNotBtn.setAttribute("id","readOrNotBtn")
-          /*  cardButton.append(readOrNotBtn)*/
             bookCard.append(readOrNotBtn)
 
+            //DELETE THE BUTTON (FOR THE TIME BEING ONLY FROM THE DOM NOT THE ARRAY)
             let deleteBtn = document.createElement('button')
             deleteBtn.setAttribute("id","deleteBtn")
                 let deleteImg = document.createElement('img')
                 deleteImg.setAttribute("id","deleteImg")
                 deleteImg.setAttribute("src","./Google Fonts/delete_FILL0_wght400_GRAD0_opsz48.svg")
             deleteBtn.append(deleteImg)
-            /*cardButton.append(deleteBtn)*/
-    bookCard.append(deleteBtn)
-        /*bookCard.append(cardButton) */
+    
+         bookCard.append(deleteBtn)
 
         libraryEl.append(bookCard)
     
@@ -77,10 +69,7 @@ Book.prototype.addBook = function (){
 
     
         //DELETE THE NEWBOOK OBJECT WHEN THE DELETE BUTTON IS CLICKED
-        deleteBtn.addEventListener('click', ()=>{
-            bookCard.remove() 
-            
-        })
+        deleteBtn.addEventListener('click', removeBook)
 
         //TOGGLE COLOR AND TEXT CONTENT WHEN THE READ BUTTON IS CLICKED
 
@@ -89,7 +78,26 @@ Book.prototype.addBook = function (){
             readOrNotBtn.classList.toggle('notRead')
         })
 
+        //REMOVE BOOK FROM DOM AND ARRAY
+function removeBook(){
+    bookCard.remove() 
+    console.log(bookCard.getAttribute('id'))
+    myLibrary.splice(bookCard.getAttribute('id'),1)
+    console.log(myLibrary)
 }
+
+//ADD
+
+function addBookToLibrary(){
+    myLibrary.push(NewBook)
+    console.log(myLibrary)
+    console.log(myLibrary.indexOf(NewBook))
+    bookCard.setAttribute("id",`${myLibrary.indexOf(NewBook)}`)
+}
+}
+//SAVE NewBook object in myLIbrary array of objects
+let myLibrary = []
+let i = 0 //index for each book 
 
 
 //CREATE A NEW BOOK AND PASS THE INFO OF THE PUPUP TO THE Book construtor
@@ -97,6 +105,15 @@ let NewBook = new Book ()
 function displayBook(x,y,z){
     NewBook = new Book (x, y, z)
 }
+
+
+
+
+
+
+
+
+
 
 
 sendPopUp.addEventListener('click',(e)=>{
@@ -110,7 +127,7 @@ sendPopUp.addEventListener('click',(e)=>{
 //EDIT EVERY NewBook taking into account if it is read or not
 const bookCheckbox = document.getElementById('readCheckbox')
 
-
+//THE FUNCTION DISPLAYS DIFFERENT COLORS IF THE CHECKBOX HAS BEEN CHECKED OR NO
 function readOrNot(item, ReadBtn, DeleteBtn){
    
     if(bookCheckbox.checked === true){
